40f3feaffb4cd5f75cfbcb10cd2fa206c73e64c9
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-11-30 16:02:05 +0200

    use lock for event listener writes; fixes #5

--- events-listener/listener.py
+++ events-listener/listener.py
@@ -4,6 +4,7 @@ import os
 
 from flask import Flask, request
 from pprint import pprint
+from threading import Lock
 
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
@@ -21,15 +22,17 @@ def get_current_filename():
 
 
 app = Flask('Elasticboard listener')
+fs_lock = Lock()
 
 
 @app.route('/', methods=['POST'])
 def got_event():
     event = json.loads(request.form['payload'])
 
+    fs_lock.acquire()
     with open(os.path.join(DATA_DIR, get_current_filename()), 'a') as f:
-        f.write(unicode(event))
-        f.write('\n')
+        f.write(unicode(event) + '\n')
+    fs_lock.release()
 
     return 'OK'
 
