f24508580f6825441894d4fc9145a5e8ca671d8c
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-11-30 16:33:49 +0200

    get gh archive data for given hour

--- data-processor/lib.py
+++ data-processor/lib.py
@@ -1,6 +1,8 @@
+import datetime
 import gzip
 import json
 import sys
+import urllib
 
 from pprint import pprint
 
@@ -33,4 +35,23 @@ def parse_events(path, owner, name, gz=False):
                 events.append(obj)
 
     return events
+
+def get_archive_data(hour):
+    """
+    Takes in a datetime.datetime object and returns
+    a path on the local filesystem to the downloaded github archive
+    raw file for the given hour.
+    """
+
+    # we don't support files before August 12, 2012
+    # https://github.com/igrigorik/githubarchive.org/issues/9#issuecomment-7670455
+    oldest = datetime.datetime(year=2012, month=8, day=12)
+    if hour < oldest:
+        raise ValueError("We don't support dates older than August 12, 2012.")
+
+    url = "http://data.githubarchive.org/%4d-%02d-%02d-%d.json.gz"
+    url = url % (hour.year, hour.month, hour.day, hour.hour)
+
+    path, headers = urllib.urlretrieve(url)
+    return path
 
