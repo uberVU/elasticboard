0a3a27a685cc99e0dce1590deded58edc701ab07
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-12-02 01:58:38 +0200

    revert to kibana default host

--- dashboard/kibana-3.0.0milestone4/config.js
+++ dashboard/kibana-3.0.0milestone4/config.js
@@ -18,7 +18,7 @@ function (Settings) {
      * elasticsearch host
      * @type {String}
      */
-    elasticsearch: "http://localhost:9200",
+    elasticsearch: "http://"+window.location.hostname+":9200",
 
     /**
      * The default ES index to use for storing Kibana specific object
