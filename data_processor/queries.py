a70095dc3a15ca23f5aab23ed1acb697299a51a7
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-12-08 00:12:39 +0200

    [query] most active contributors; #29

--- data_processor/queries.py
+++ data_processor/queries.py
@@ -2,5 +2,17 @@ from es import ES
 
 import elasticsearch
 
+SIZE = 100
+
 # queries go here
+def most_active_people():
+    body = {'query':
+                {'match_all': {}},
+            'facets':
+                {'people': {
+                    'terms': {
+                        'field': 'actor.login',
+                        'size': SIZE}}}}
+    res = ES.search(body=body)
+    return res['facets']['people']['terms']
 
