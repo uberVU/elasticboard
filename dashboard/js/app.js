328b1e6f1d2d82b6d92eafc2648b46f14e617ecd
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-12-15 19:42:46 +0200

    mark generic graph func intention

--- dashboard/js/app.js
+++ dashboard/js/app.js
@@ -5,6 +5,7 @@ function logFailure(fail) {
     console.log(fail);
 }
 
+// might have to extract a makeGraph function that takes in "bar" as a graph type
 function makeBarGraph(container, endpoint, keyName, valueName) {
     $.getJSON(API_BASE + endpoint)
         .done(function (json) {
