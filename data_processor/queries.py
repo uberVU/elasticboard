802da4acddbb21da4d3fe0150ad783ba5b84dc1c
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-12-15 19:09:02 +0200

    clean up queries file

--- data_processor/queries.py
+++ data_processor/queries.py
@@ -4,10 +4,11 @@ from elasticutils import S
 import datetime
 import elasticsearch
 
-SIZE = 100
-DATEFORMAT = '%Y-%m-%dT%H:%M:%SZ'
-
 def fit_time_range(start, end):
+    """
+    Keeps an empty time range empty. Stretches an incomplete
+    time range.
+    """
     if start == None and end == None:
         return (None, None)
     if start == None:
@@ -20,6 +21,8 @@ def fit_time_range(start, end):
 def most_active_people(index, start=None, end=None):
     """
     Finds the most active users - as actors in all the events.
+    -- index looks like "USER-REPO"
+
     Returns a list of dicts like:
     {'count': N, 'term': NAME}
     """
