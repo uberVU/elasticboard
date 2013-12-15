e14951788876a594222394a765b66e6e88689831
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-12-15 19:40:19 +0200

    log failures

--- dashboard/js/app.js
+++ dashboard/js/app.js
@@ -1,33 +1,40 @@
 var API_BASE = 'http://127.0.0.1:5000/';
 
+function logFailure(fail) {
+    console.log("Trouble getting data. API server down?");
+    console.log(fail);
+}
+
 function makeBarGraph(container, endpoint, keyName, valueName) {
-    $.getJSON(API_BASE + endpoint).done(function (json) {
-        data = json.data;
-        $(container).highcharts({
-            chart: {
-                type: 'bar'
-            },
-            title: {
-                text: 'Most active people'
-            },
-            xAxis: {
-                categories: data.map(function (e) { return e[keyName]; })
-            },
-            yAxis: {
-                min: 0,
+    $.getJSON(API_BASE + endpoint)
+        .done(function (json) {
+            data = json.data;
+            $(container).highcharts({
+                chart: {
+                    type: 'bar'
+                },
                 title: {
-                    text: "Events"
-                }
-            },
-            legend: {
-                enabled: false
-            },
-            series: [{
-                name: "Events",
-                data: data.map(function (e) { return e[valueName]; })
-            }]
-        });
-    });
+                    text: 'Most active people'
+                },
+                xAxis: {
+                    categories: data.map(function (e) { return e[keyName]; })
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: "Events"
+                    }
+                },
+                legend: {
+                    enabled: false
+                },
+                series: [{
+                    name: "Events",
+                    data: data.map(function (e) { return e[valueName]; })
+                }]
+            });
+        })
+        .fail(logFailure);
 }
 
 makeBarGraph('#most-active-people', 'gabrielfalcao/lettuce/most_active_people', 'term', 'count');
