43386cfa193b2338f774e34fe9701007a381e599
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-12-15 20:57:22 +0200

    total events monthly api call

--- data_processor/api.py
+++ data_processor/api.py
@@ -9,6 +9,7 @@ app.debug = True
 def index_name(user, repo):
     return '-'.join((user, repo))
 
+
 # api endpoints that call the queries
 
 @app.route('/<user>/<repo>/most_active_people')
@@ -16,6 +17,13 @@ def index_name(user, repo):
 def most_active_people(user, repo):
     index = index_name(user, repo)
     data = queries.most_active_people(index)
+    return jsonify(data=data)
+
+@app.route('/<user>/<repo>/total_events_monthly')
+@crossdomain(origin='*')
+def total_events_monthly(user, repo):
+    index = index_name(user, repo)
+    data = queries.past_n_months(index, queries.total_events, 6)
     return jsonify(data=data)
 
 
