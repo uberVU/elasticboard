b5e49533ef6ea98f61a938be83cf0f0587a23f7c
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2014-01-21 18:20:31 +0200

    update kibana

--- dashboard/kibana-latest/app/panels/fields/module.js
+++ dashboard/kibana-latest/app/panels/fields/module.js
@@ -1,4 +1,4 @@
-/*! kibana - v3.0.0pre-milestone5 - 2013-11-19
- * Copyright (c) 2013 Rashid Khan; Licensed Apache License */
+/*! kibana - v3.0.0pre-milestone5 - 2014-01-19
+ * Copyright (c) 2014 Rashid Khan; Licensed Apache License */
 
 define("panels/fields/module",["angular","app","underscore"],function(a,b,c){var d=a.module("kibana.panels.fields",[]);b.useModule(d),d.controller("fields",["$scope",function(a){a.panelMeta={status:"Deprecated",description:"You should not use this table, it does not work anymore. The table panel nowintegrates a field selector. This module will soon be removed."};var b={style:{},arrange:"vertical",micropanel_position:"right"};c.defaults(a.panel,b),a.init=function(){}}])});
