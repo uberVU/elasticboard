4e680ac8e6f3e7a25f1bc726bd95dfc20c5f344d
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2014-02-01 19:41:06 +0200

    less space at the bottom

--- dashboard/js/app.js
+++ dashboard/js/app.js
@@ -1,7 +1,7 @@
 var API_BASE = 'http://' + window.location.hostname + ':5000/';
 
 function fitTabContainer () {
-    $('#tab-container').height($(window).height() - $('#topbar').height() - 80);
+    $('#tab-container').height($(window).height() - $('#topbar').height() - 30);
 }
 window.onresize = fitTabContainer;
 fitTabContainer();
