156c781b76294f08289507832b3657545203f644
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2014-02-01 20:22:42 +0200

    load kibana lazily

--- dashboard/js/app.js
+++ dashboard/js/app.js
@@ -1,5 +1,9 @@
 var API_BASE = 'http://' + window.location.hostname + ':5000/';
 
+function loadKibana() {
+    $('#kibana-iframe').attr('src', 'kibana-latest/index.html');
+}
+
 function fitTabContainer () {
     $('#tab-container').height($(window).height() - $('#topbar').height() - 30);
 }
