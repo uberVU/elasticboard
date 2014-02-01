d76257b709bf041004466bb07b4850ff8c1549d5
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2014-02-01 17:37:38 +0200

    tab switching context

--- dashboard/js/tabs.js
+++ dashboard/js/tabs.js
@@ -1,19 +1,34 @@
 var $crtTab = $('#tab-1');
 
 $('ul.menu li').click(function (e) {
+    /* changes tabs;
+     * saves scroll context
+     * calls init function (data-function) the first time
+     */
+
     $t = $(e.target);
     var tabID = $t.data('tab');
     var $newTab = $('#' + tabID);
     var $tabContainer = $('#tab-container');
+
+    var scroll = $crtTab.scrollTop();
+    $crtTab.data('scroll', scroll);
+
     $crtTab.hide();
     $newTab.show();
-    $crtTab = $newTab;
 
-    $newTab.scrollTop(0);
+    var newScroll = $newTab.data('scroll');
+    if (!newScroll) {
+        $newTab.scrollTop(0);
+    }
+
+    $crtTab.data('init', true);
 
     // call function (if any)
     var fname = $t.data('function');
-    if (fname) {
+    if (fname && !$newTab.data('init')) {
         window[fname]();
     }
+
+    $crtTab = $newTab;
 });
