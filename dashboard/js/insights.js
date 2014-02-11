e670e8fd1703addee06b7faea4501a4cb7e0a000
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2014-02-11 17:02:10 +0100

    extract function for issues activity

--- dashboard/js/insights.js
+++ dashboard/js/insights.js
@@ -1,4 +1,4 @@
-function drawInsights () {
+function drawIssuesActivity() {
     $.getJSON(API_BASE + '/issues_activity')
         .done(function (json) {
             data = json.data;
@@ -26,26 +26,31 @@ function drawInsights () {
                 legend: {
                     enabled: false
                 },
-                series: [{
-                    name: 'Opened',
-                    data: opened.reduceRight(function (arr, el) {
-                        arr.push(el.value);
-                        return arr;
-                    }, []),
-                    lineColor: '#FF4E50',
-                    color: '#FF4E50'
-                }, {
-                    name: 'Closed',
-                    data: closed.reduceRight(function (arr, el) {
-                        arr.push(el.value);
-                        return arr;
-                    }, []),
-                    lineColor: '#88C425',
-                    color: '#88C425'
-                }]
+                series: [
+                    {
+                        name: 'Opened',
+                        data: opened.reduceRight(function (arr, el) {
+                            arr.push(el.value);
+                            return arr;
+                        }, []),
+                        lineColor: '#FF4E50',
+                        color: '#FF4E50'
+                    },
+                    {
+                        name: 'Closed',
+                        data: closed.reduceRight(function (arr, el) {
+                            arr.push(el.value);
+                            return arr;
+                        }, []),
+                        lineColor: '#88C425',
+                        color: '#88C425'
+                    }
+                ]
             });
         })
         .fail(logFailure);
+}
 
-
+function drawInsights () {
+    drawIssuesActivity();
 }
