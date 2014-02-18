b92a919bf9ac232bb2b5f9425c0bf122c0975752
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2014-02-18 18:23:14 +0100

    add file to be called by cronjob

---
+++ remove_temporary_rivers.py
@@ -0,0 +1,26 @@
+from data_processor.queries import *
+import sys
+import time
+
+# python remove_temporary_rivers.py [--all]
+
+LIFETIME = 3600 # one hour
+
+REMOVE_ALL = False
+if len(sys.argv) == 2 and sys.argv[1] == '--all':
+    REMOVE_ALL = True
+
+rivers = S().indexes('_river') \
+    .filter(type='github') \
+    .values_dict()
+
+for river in rivers:
+    if 'temporary' in river:
+        ctime = river['created_at']
+        now = time.time()
+        if REMOVE_ALL or (now - ctime > LIFETIME):
+            owner = river['github']['owner']
+            repository = river['github']['repository']
+            index_name = '%s&%s' % (owner, repository)
+            ES.indices.delete(index_name)
+            ES.transport.perform_request(url='/_river/%s' % index_name, method='DELETE')
