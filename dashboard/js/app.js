adc3447777211756b078096c3997f0b724b52926
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2014-02-20 00:24:45 +0100

    display a message when there's no data; fixes #59

--- dashboard/js/app.js
+++ dashboard/js/app.js
@@ -83,7 +83,20 @@ $('#repo-select-trigger').on('click', function (e) {
 
 function getAvailableRepos (cb) {
     $.get(API_HOST + 'available_repos')
-        .success(cb)
+        .success(function (data) {
+            if (data.data.length == 0) {
+                $('#counts-container').remove();
+                $('#tab-container').empty();
+
+                var msg = "<p class=\"text-center\">No data available. Please follow the " +
+                    "<a href=\"https://github.com/uberVU/elasticboard/blob/master/README.md\">README</a>.</p>";
+                $tabContainer = $('#tab-container');
+                $tabContainer.append(msg);
+
+                return;
+            }
+            cb(data);
+        })
     .fail(function (data) {
         console.log('An error has occured');
     });
