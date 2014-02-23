d7c391cb37ec75a85093a0c45e9bfac9a2da0007
Andrei Oprea <andrei.br92@gmail.com>
2014-02-23 22:45:52 +0200

    Refactor autocomplete

--- dashboard/js/try.js
+++ dashboard/js/try.js
@@ -1,8 +1,15 @@
 var data = null;
+var timeoutOwner = null;
+var timeoutRepository = null;
+var owner = '';
+var $inputOwner = $('.input--owner');
+var $inputRepository = $('.input--repository');
+
 function processUserRepos(owner) {
     var URL = 'https://api.github.com/users/' + owner + '/repos?per_page=1000';
 
-    $.getJSON(URL).done(function(d) {
+    $.getJSON(URL)
+      .success(function(d) {
         data = d;
 
         var repos = new Bloodhound({
@@ -14,35 +21,23 @@ function processUserRepos(owner) {
 
         repos.initialize();
 
-        $('.input--repository').typeahead('destroy');
-        $('.input--repository').typeahead(null, {
+        $inputRepository.typeahead('destroy');
+        $inputRepository.typeahead(null, {
             name: 'repository',
             displayKey: 'name',
             source: repos.ttAdapter()
         });
 
         $inputRepository.focus();
+    })
+    .fail(function(d) {
+        console.log(d);
+        // TODO handle fail, probably bad username?
     });
 }
 
-var timeoutOwner = null;
-var timeoutRepository = null;
-var owner = '';
-var $inputOwner = $('.input--owner');
-var $inputRepository = $('.input--repository');
-
 $inputOwner.on('keyup change', function(e) {
     $inputRepository.on('focus', triggerAutocomplete);
-    if (timeoutOwner) {
-        window.clearTimeout(timeoutOwner);
-    }
-    timeoutOwner = window.setTimeout(function() {
-        var $input = $(e.target);
-
-        owner = $input.val().trim();
-
-        // TODO check if user exists and display some feedback
-    }, 200);
 });
 
 function triggerAutocomplete(e) {
@@ -51,28 +46,33 @@ function triggerAutocomplete(e) {
 }
 
 $inputRepository.on('keyup change', function(e) {
+    $input = $(this);
+
     if (timeoutRepository) {
         window.clearTimeout(timeoutRepository);
     }
     timeoutRepository = window.setTimeout(function() {
-        var $input = $(e.target);
         var name = $input.val().trim();
 
         if (!data || !name) {
             return;
         }
 
-        var ok = false;
-        data.forEach(function(r) {
-            if (r.name.toLowerCase() === name.toLowerCase()) {
-                ok = true;
-            }
+        var ok = data.some(function(r) {
+            return r.name.toLowerCase() === name.toLowerCase();
         });
 
         if (ok) {
-            // TODO: show GO button
-            console.log("exists");
+          showDashboardLink();
         }
 
     }, 200);
 });
+
+function showDashboardLink() {
+
+    var link = $('<a/>').attr('href', '#').text('Go to the dashboard');
+    var h2 = $('<h2/>').append(link);
+    $('.form').html('').append(h2);
+
+}
